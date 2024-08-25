9c2ffa4ccc559a9c2499d950292281dd368139cc
David Afonso <davafons@gmail.com>
2024-08-25 20:06:57 +0900

    Handle env variable properly

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,6 @@ plugins {
   id("org.jlleitschuh.gradle.ktlint") version "12.1.0"
   id("com.github.ben-manes.versions") version "0.50.0"
   id("org.jreleaser") version "1.10.0"
-  id("co.uzzu.dotenv.gradle") version "4.0.0"
 }
 
 fun isNonStable(version: String): Boolean {
